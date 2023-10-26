1e91898d84a77ca46c8ad9e5142864f519b453a7
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-26 19:45:34 -0400

    Time: 4880 ms (6.57%) | Memory: 17 MB (94.90%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,9 +4,9 @@ class Solution:
         for index in range(len(nums)):
             # Go through the array for the second number
             for secondIndex in range(len(nums)):
-                # Make sure the index of the first and second number are not the same
-                if (index == secondIndex):
-                    continue
                 # If they equal the target, then return the indexes
                 if (nums[index] + nums[secondIndex] == target):
-                    return index, secondIndex
+                    # Make sure the index of the first and second number are not the same
+                    if (index != secondIndex):
+                        return index, secondIndex
+        return []
