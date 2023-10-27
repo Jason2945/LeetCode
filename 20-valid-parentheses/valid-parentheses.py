02248df58f37129996d83f9cca6de8ee69c7f682
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-26 21:29:49 -0400

    Time: 40 ms (47.72%) | Memory: 16.3 MB (27.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -2,20 +2,26 @@ class Solution:
     def isValid(self, s: str) -> bool:
         openBrackets = ""
         closing = ['}', ']', ')']
+        # Run through the string
         for chars in s:
+            # Check to make sure the first character isnt a closing bracket
             if chars in closing:
                 if len(openBrackets) < 1:
                     return False
+            # Check to see if it is open bracket, if it is add it to a new string
             if (chars == '{' or chars == '[' or chars == '('):
                 openBrackets += chars
+            # Check to make sure that when closing, the previous one matches
             elif (chars == "}" and openBrackets[-1] != "{"):
                 return False
             elif (chars == "]" and openBrackets[-1] != "["):
                 return False
             elif (chars == ")" and openBrackets[-1] != "("):
                 return False
+            # Make sure all opened brackets are closed
             else :
                 openBrackets = openBrackets[:-1]
+        # Make sure there are nothing left and all brackets are closed
         if openBrackets == "":
             return True
         else :
