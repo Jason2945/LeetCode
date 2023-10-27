73b8acfcaf2cb277b0d7b3f192a972ad5b67c703
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-26 22:37:52 -0400

    Time: 45 ms (12.70%) | Memory: 16.3 MB (43.85%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,9 +1,11 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        # Check to make sure the needle is in the haystack
-        if needle in haystack:
-            index = haystack.find(needle)
-            return index
-        else :
-            return -1
+        # Make sure the length of haystack is longer than the length of the needle.
+        # You only need to check when the needle can still fit in the haystack
+        #IE if needle has 7 letters and haystack has 3 letters left, it won't fit the needle so no need to check
+        for index in range(len(haystack) - len(needle) + 1):
+            #Check that haystack to the length to the length of the needle if is there, return the first index appeared
+            if haystack[index : index+len(needle)] == needle:
+                return index
+        return -1
         
