bfd644072bbf0e366de957e70c0ee0e413effc48
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-26 22:06:20 -0400

    Time: 105 ms (18.77%) | Memory: 17.8 MB (82.60%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -2,7 +2,11 @@ class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         noDupeIndex = 0
         numsChecked = []
+        # Run through every item in the list
         for index in range(len(nums)):
+            # If the number is not a duplicate, move it to the front of the most unique number
+            # Add it to the list of numbers that appeared
+            # Increase the counter of the amount of unseen duplicates
             if nums[index] not in numsChecked:
                 numsChecked.append(nums[index])
                 nums[noDupeIndex] = nums[index]
