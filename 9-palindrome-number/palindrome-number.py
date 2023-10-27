373e688296b8809b45c5a9ba9e987309bfd5a4a4
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-26 20:07:21 -0400

    Time: 65 ms (38.36%) | Memory: 16.4 MB (15.47%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,13 +1,7 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # Change x into a string
-        number = str(x)
-        # Create a single empty string to check if it is palindrome
-        palindrome_check = ""
-        # Put the digits in number in reverse to the palindrome check and compare if they are the same
-        for dig in reversed(number):
-            palindrome_check += dig
-        if palindrome_check == number:
-            return True
-        else:
+        if (x < 0):
             return False
+        newX = str(x)[::-1]
+        if newX == str(x):
+            return True
