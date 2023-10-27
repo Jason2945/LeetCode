5f72da8f02065d26b4c26b4deba32f6f9ca7197d
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-26 20:46:45 -0400

    Time: 39 ms (73.38%) | Memory: 16.3 MB (43.78%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,19 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        common_prefix = ""
-        # Find the smallest word in strs
-        shortest_word = min(strs, key=len)
-        
-        # Get the length of the shortest word and use it as the index to test for all words in strs
-        for word_index in range(len(shortest_word)):
-            # Check each worth in strs
-            for word in strs:
-                # If the letter doesn't match the shortest word's index, return the current common prefix
-                if word[word_index] != shortest_word[word_index]:
-                    return common_prefix
-            # If it matches, add the current word_index into common prefix and test the next letter
-            common_prefix += shortest_word[word_index]
-        # Return common prefix after all the test is done
-        return common_prefix
+        longestCommonPre = ""
+        for index in range(len(min(strs, key=len))):
+            letter = strs[0][index]
+            for words in strs:
+                if words[index] != letter:
+                    return longestCommonPre
+            longestCommonPre += letter
+        return longestCommonPre
 
         
