26e95fb10f1b4dd28d3c0c81d6d443b5295c6dc8
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2023-10-28 14:11:13 -0400

    Time: 40 ms (64.29%) | Memory: 16.3 MB (23.74%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,25 +1,34 @@
 class Solution:
+    # Done it without bin method
     def addBinary(self, a: str, b: str) -> str:
-        # Create a var to hold a carry over
-        carry_over = 0
-        # Add the numbers together
-        ab_sum = int(a) + int(b)
-        digit_list = [int(digit) for digit in str(ab_sum)]
-        # Start at the last index and move to the first
-        for index in range(len(digit_list)-1, -1 , -1):
-            if carry_over == 1:
-                if digit_list[index] == 2:
-                    digit_list[index] = 1
-                elif digit_list[index] == 1:
-                    digit_list[index] = 0
+        binarySum = ""
+        holder = 0
+        intArr = list(str(int(a) + int(b)))
+        for index in range(len(intArr)-1, -1 ,-1):
+            if holder == 1:
+                if intArr[index] == '2':
+                    intArr[index] = '1'
+                elif intArr[index] == '1':
+                    intArr[index] = '0'
                 else :
-                    digit_list[index] = 1
-                    carry_over = 0
+                    intArr[index] = '1'
+                    holder = '0'
+                if index == 0:
+                        intArr = ['1'] + intArr
+                        binarySum = ''.join(intArr)
+                        return binarySum
             else :
-                if digit_list[index] == 2:
-                    digit_list[index] = 0
-                    carry_over = 1
-        if carry_over == 1:
-            digit_list = [1] + digit_list
-        binary_value = "".join(str(digits) for digits in digit_list)
-        return(binary_value)
+                if intArr[index] == '2':
+                    intArr[index] = '0'
+                    holder = 1
+                    if index == 0:
+                        intArr = ['1'] + intArr
+                        binarySum = ''.join(intArr)
+                        return binarySum
+                if index == 0:
+                    binarySum = ''.join(intArr)
+                    return binarySum
+
+
+
+
