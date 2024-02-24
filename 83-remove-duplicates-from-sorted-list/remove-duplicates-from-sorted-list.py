1bf55f39de2b0807189fe84d61f010d0ee1e3244
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2024-02-24 11:29:57 -0500

    Time: 41 ms (54.89%) | Memory: 16.6 MB (47.02%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -1,17 +1,22 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # If head is empty return nothing
+        if head == None:
+            return
         temp = head
-        while temp and temp.next:
-            if temp.val == temp.next.val:
+        # Check to see if the current val is none or if the next val is none end the while loop
+        while (temp and temp.next):
+            # Check the current value to the next value
+            # If the value are the same skip the next value
+            if (temp.val == temp.next.val):
                 temp.next = temp.next.next
+            # If the next value is not the same as current, proceed to the next value
             else :
                 temp = temp.next
+        # Return the head from the beginning
         return head
-
-
-        
