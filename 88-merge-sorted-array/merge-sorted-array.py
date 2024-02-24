8e09d7f05ede9dee57bbb57b1101a37b8b2c0df8
Jason Wang <86026175+Jason2945@users.noreply.github.com>
2024-02-24 12:04:02 -0500

    Time: 34 ms (90.45%) | Memory: 16.6 MB (88.07%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -2,22 +2,31 @@ class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
+
+        Notes: 
+        m is the number that needs to be merged from nums1
+        n is the number that needs to be combined from nums2
+        m+n is greater than and equal to 1
+        m or n itself can be 0
+
+        Steps to solving this
+        1. Check if m or n is 0. if m is 0, just set nums1 to equal nums2
+        2. If n is 0 no need to do anything
+        3. Check from the back to the front, this way it will be sorted from smallest to greatest
         """
-        total_len = m + n
-        # Make sure that there is elements in nums2 or else don't do anything
-        if n == 0:
-            pass
-        else :
-            # Run through all the values in nums1
-            for index in range(total_len):
-                # Skip all the elements in m that does not need to be replaced
-                if index <= m-1:
-                    pass
-                # Replace the holder values in nums1 with nums2 elements
-                else :
-                    nums1[index] = nums2[n-1]
-                    # Subtract the number of values that need to be added to nums1
-                    n -= 1
-            # Sort the number into non-decreasing order
-            nums1.sort()
+        # Do the while loop until there is no more number in nums2
+        while n > 0:
+            # If there are no more values to be moved from nums1, slice the remainder numbers in nums2 to nums1
+            if m == 0:
+                nums1[:n] = nums2[:n]
+                break
+            # If nums1 number is greater than nums2 number, put nums1 number at the back and subtract 1 from m
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m = m-1
+            # If nums2 number is greater or equal to nums1 number, put nums2 number at the back and subtract 1 from n
+            elif nums1[m-1] <= nums2[n-1]:
+                nums1[m+n-1] = nums2[n-1]
+                n = n-1
+
         
